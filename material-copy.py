eca4418af9b7043a54cbcd2350d796bcf0c7e77f
Jonathan Mackenzie <jonmac1@gmail.com>
2015-05-25 15:25:05 +0930

    Update material-copy.py
    added valid choices for color

--- material-copy.py
+++ material-copy.py
@@ -5,7 +5,7 @@ import os
 parser = argparse.ArgumentParser(description='Copy Image Assets from Material Design to your Android Project, preview all here: https://google.github.io/material-design-icons/, place and run this script inside the material-icons-master folder')
 parser.add_argument('group', type=str, help='The group eg. social, av, navigation')
 parser.add_argument('name', type=str, help='The name of the icon eg. 3d_rotation, attach_file, poll')
-parser.add_argument('color', type=str, help='black or white')
+parser.add_argument('color', type=str, help='color of the icon',choices=['black','white'])
 parser.add_argument('dest', type=str, help='Desination res folder')
 args = parser.parse_args()
 dirs = map(lambda x: args.group+'/drawable-'+x+'/', ['hdpi', 'mdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi'])
